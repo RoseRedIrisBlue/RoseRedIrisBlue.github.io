81754afe3cc2fb30a2272fd42b8885f15ec78887
KuraiTenshi26 <34247990+KuraiTenshi26@users.noreply.github.com>
2019-06-26 12:16:52 -0400

    lickitung tuning

--- mods/gen1/pokedex.js
+++ mods/gen1/pokedex.js
@@ -1488,7 +1488,7 @@ exports.BattlePokedex = {
 		species: "Lickitung",
 		types: ["Normal"],
 		gender: "N",
-		baseStats: {hp: 110, atk: 85, def: 85, spa: 75, spd: 75, spe: 50},
+		baseStats: {hp: 90, atk: 75, def: 85, spa: 75, spd: 75, spe: 50},
 		abilities: {},
 		heightm: 1.2,
 		weightkg: 65.5,
