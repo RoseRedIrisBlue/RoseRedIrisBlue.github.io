b28792b81f2d0e0ff8ef0270135b4d1a43d62e98
KuraiTenshi26 <34247990+KuraiTenshi26@users.noreply.github.com>
2019-06-27 18:21:20 -0400

    first commit

---
+++ js/data/setdex_rby.js
@@ -0,0 +1,259 @@
+var SETDEX_RBY = {
+	"Aerodactyl": {
+		"Physical Attacker": {"level": 100, "moves": ["Whirlwind", "Rock Slide", "Fire Blast", "Hyper Beam"]},
+	},
+	"Alakazam": {
+		"Standard": {"level": 100, "moves": ["Psychic", "Recover", "Thunder Wave", "Seismic Toss"]},
+	},
+	"Arbok": {
+		"Glare": {"level": 100, "moves": ["Acid", "Earthquake", "Glare", "Hyper Beam"]},
+	},
+	"Arcanine": {
+		"Mixed": {"level": 100, "moves": ["Fire Blast", "Body Slam", "Hyper Beam", "Reflect"]},
+	},
+	"Articuno": {
+		"Mixed": {"level": 100, "moves": ["Blizzard", "Whirlwind", "Agility", "Hyper Beam"]},
+	},
+	"Beedrill": {
+		"Swords Dance": {"level": 100, "moves": ["Twineedle", "Poison Sting", "Dream Eater", "Swords Dance"]},
+	},
+	"Blastoise": {
+		"Offensive Tank": {"level": 100, "moves": ["Hydro Pump", "Earthquake", "Blizzard", "Submission"]},
+	},
+	"Butterfree": {
+		"Psychic Killer": {"level": 100, "moves": ["Psychic", "Leech Life", "Sleep Powder", "Stun Spore"]},
+	},
+	"Chansey": {
+		"Special Wall": {"level": 100, "moves": ["Ice Beam", "Soft-Boiled", "Thunder Wave", "Thunderbolt"]},
+	},
+	"Charizard": {
+		"Swords Dance": {"level": 100, "moves": ["Fire Blast", "Earthquake", "Hyper Fang", "Hyper Beam"]},
+	},
+	"Clefable": {
+		"Offensive": {"level": 100, "moves": ["Blizzard", "Body Slam", "Hyper Beam", "Softboiled"]},
+		"Status Spreader": {"level": 100, "moves": ["Blizzard", "Thunder Wave", "Thunderbolt", "Softboiled"]},
+	},
+	"Cloyster": {
+		"Clamp": {"level": 100, "moves": ["Clamp", "Blizzard", "Tri Attack", "Explosion"]},
+	},
+	"Dewgong": {
+		"Amnesia": {"level": 100, "moves": ["Surf", "Blizzard", "Amnesia", "Rest"]},
+	},
+	"Ditto": {
+		"Transformer": {"level": 100, "moves": ["Transform"]},
+	},
+	"Dodrio": {
+		"Physical Attacker": {"level": 100, "moves": ["Drill Peck", "Body Slam", "Hyper Beam", "Jump Kick"]},
+	},
+	"Dragonite": {
+		"Attacker": {"level": 100, "moves": ["Agility", "Wrap", "Hyper Beam", "Hyper Fang"]},
+	},
+	"Dugtrio": {
+		"Physical Cleaner": {"level": 100, "moves": ["Earthquake", "Rock Slide", "Slash", "Hyper Beam"]},
+	},
+	"Electabuzz": {
+		"Mixed Attacker": {"level": 100, "moves": ["Thunderbolt", "Thunder Wave", "Psychic", "Submission"]},
+	},
+	"Electrode": {
+		"Offensive Utility": {"level": 100, "moves": ["Thunderbolt", "Thunder Wave", "Explosion", "Hyper Beam"]},
+	},
+	"Exeggutor": {
+		"Standard": {"level": 100, "moves": ["Sleep Powder", "Psychic", "Explosion", "Mega Drain"]},
+	},
+	"Farfetch'd": {
+		"GOAT": {"level": 100, "moves": ["Swords Dance", "Whirlwind", "Razor Leaf", "Slash"]},
+	},
+	"Fearow": {
+		"Dream Eater": {"level": 100, "moves": ["Hypnosis", "Drill Peck", "Tri Attack", "Dream Eater"]},
+	},
+	"Flareon": {
+		"Bulky Attacker": {"level": 100, "moves": ["Fire Blast", "Body Slam", "Hyper Beam", "Quick Attack"]},
+	},
+	"Gengar": {
+		"Dream Eater": {"level": 100, "moves": ["Hypnosis", "Explosion", "Thunderbolt", "Dream Eater"]},
+	},
+	"Golbat": {
+		"UU Screech": {"level": 100, "moves": ["Screech", "Confuse Ray", "Double-Edge", "Mega Drain"]},
+	},
+	"Golduck": {
+		"Amnesia": {"level": 100, "moves": ["Amnesia", "Blizzard", "Surf", "Tri Attack"]},
+	},
+	"Golem": {
+		"Standard": {"level": 100, "moves": ["Earthquake", "Rock Slide", "Explosion", "Body Slam"]},
+	},
+	"Graveler": {
+		"UU Showdown Usage": {"level": 100, "moves": ["Earthquake", "Explosion", "Rock Slide", "Body Slam"]},
+	},
+	"Gyarados": {
+		"Mixed Attacker": {"level": 100, "moves": ["Splash", "Hydro Pump", "Hyper Beam", "Thunderbolt"]},
+	},
+	"Haunter": {
+		"UU Showdown Usage": {"level": 100, "moves": ["Explosion", "Hypnosis", "Thunderbolt", "Psychic"]},
+	},
+	"Hitmonchan": {
+		"Mixed Punches": {"level": 100, "moves": ["Submission", "Ice Punch", "Thunder Punch", "Body Slam"]},
+	},
+	"Hitmonlee": {
+		"Physical Attacker": {"level": 100, "moves": ["Body Slam", "Double-Edge", "High Jump Kick", "Seismic Toss"]},
+	},
+	"Hypno": {
+		"Dream Eater": {"level": 100, "moves": ["Psychic", "Submission", "Hypnosis", "Dream Eater"]},
+	},
+	"Jolteon": {
+		"Special Sweeper": {"level": 100, "moves": ["Thunderbolt", "Thunder Wave", "Pin Missile", "Body Slam"]},
+	},
+	"Jynx": {
+		"Lovely Kiss": {"level": 100, "moves": ["Lovely Kiss", "Blizzard", "Psychic", "Mimic"]},
+	},
+	"Kabutops": {
+		"Swords Dance": {"level": 100, "moves": ["Swords Dance", "Rock Slide", "Surf", "Hyper Beam"]},
+	},
+	"Kadabra": {
+		"UU Showdown Usage": {"level": 100, "moves": ["Psychic", "Thunder Wave", "Recover", "Seismic Toss"]},
+	},
+	"Kangaskhan": {
+		"Physical Attacker": {"level": 100, "moves": ["Earthquake", "Body Slam", "Hyper Beam", "Rock Slide"]},
+	},
+	"Kingler": {
+		"Swords Dance": {"level": 100, "moves": ["Swords Dance", "Hyper Beam", "Crabhammer", "Vice Grip"]},
+	},
+	"Lapras": {
+		"Parafusion": {"level": 100, "moves": ["Blizzard", "Thunderbolt", "Body Slam", "Confuse Ray"]},
+	},
+	"Lickitung": {
+		"Swords Dance": {"level": 100, "moves": ["Swords Dance", "Earthquake", "Hyper Beam", "Body Slam"]},
+	},
+	"Machamp": {
+		"Physical Attacker": {"level": 100, "moves": ["Body Slam", "Earthquake", "Hyper Beam", "Submission"]},
+	},
+	"Magmar": {
+		"Mixed Attacker": {"level": 100, "moves": ["Fire Blast", "Psychic", "Confuse Ray", "Submission"]},
+	},
+	"Magneton": {
+		"Wall": {"level": 100, "moves": ["Thunder", "Thunder Wave", "Recover", "Toxic"]},
+	},
+	"Marowak": {
+		"Physical Wallbreaker": {"level": 100, "moves": ["Earthquake", "Rock Slide", "Body Slam", "Hyper Beam"]},
+	},
+	"Mew": {
+		"Swords Dance": {"level": 100, "moves": ["Swords Dance", "Earthquake", "Hyper Beam", "Soft-Boiled"]},
+		"Special Attacker": {"level": 100, "moves": ["Thunderbolt", "Blizzard", "Soft-Boiled", "Psychic"]},
+	},
+	"Mewtwo": {
+		"All-Out Attacker": {"level": 100, "moves": ["Psychic", "Blizzard", "Submission", "Self-Destruct"]},
+	},
+	"Moltres": {
+		"Agility + Fire Spin": {"level": 100, "moves": ["Agility", "Fire Blast", "Fire Spin", "Whirlwind"]},
+	},
+	"Mr. Mime": {
+		"Offensive": {"level": 100, "moves": ["Psychic", "Thunder Wave", "Thunderbolt", "Seismic Toss"]},
+	},
+	"Muk": {
+		"Physical Attacker": {"level": 100, "moves": ["Sludge", "Body Slam", "Screech", "Explosion"]},
+	},
+	"Nidoking": {
+		"Lovely Kiss": {"level": 100, "moves": ["Earthquake", "Blizzard", "Thunder", "Lovely Kiss"]},
+	},
+	"Nidoqueen": {
+		"Wall": {"level": 100, "moves": ["Earthquake", "Blizzard", "Body Slam", "Softboiled"]},
+	},
+	"Ninetales": {
+		"ParaFusion": {"level": 100, "moves": ["Fire Blast", "Psychic", "Thunder Wave", "Confuse Ray"]},
+	},
+	"Omastar": {
+		"Wall": {"level": 100, "moves": ["Hydro Pump", "Blizzard", "Recover", "Body Slam"]},
+	},
+	"Onix": {
+		"Physical Attacker": {"level": 100, "moves": ["Earthquake", "Explosion", "Rock Slide", "Bind"]},
+	},
+	"Parasect": {
+		"Spore": {"level": 100, "moves": ["Spore", "Stun Spore", "Leech Life", "Mega Drain"]},
+	},
+	"Persian": {
+		"Hypnosis": {"level": 100, "moves": ["Slash", "Bubble Beam", "Hyper Beam", "Hypnosis"]},
+	},
+	"Pidgeot": {
+		"Softboiled": {"level": 100, "moves": ["Sing", "Hyper Beam", "Whirlwind", "Softboiled"]},
+	},
+	"Pinsir": {
+		"Swords Dance": {"level": 100, "moves": ["Swords Dance", "Hyper Beam", "Vice Grip", "Submission"]},
+	},
+	"Poliwrath": {
+		"Dream Eater": {"level": 100, "moves": ["Hypnosis", "Dream Eater", "Surf", "Submission"]},
+	},
+	"Porygon": {
+		"Sharpen Sweeper": {"level": 100, "moves": ["Sharpen", "Hyper Beam", "Tri Attack", "Recover"]},
+	},
+	"Primeape": {
+		"Physical Sweeper": {"level": 100, "moves": ["Karate Chop", "Rock Slide", "Body Slam", "Hyper Beam"]},
+	},
+	"Raichu": {
+		"Standard": {"level": 100, "moves": ["Thunderbolt", "Thunder Wave", "Surf", "Submission"]},
+	},
+	"Rapidash": {
+		"Fire Spin": {"level": 100, "moves": ["Fire Blast", "Agility", "Hyper Beam", "Fire Spin"]},
+	},
+	"Raticate": {
+		"Physical Sweeper": {"level": 100, "moves": ["Super Fang", "Body Slam", "Hyper Beam", "Bubble Beam"]},
+	},
+	"Rhydon": {
+		"Offensive Tank": {"level": 100, "moves": ["Earthquake", "Rock Slide", "Submission", "Body Slam"]},
+	},
+	"Sandslash": {
+		"Swords Dance": {"level": 100, "moves": ["Earthquake", "Swords Dance", "Hyper Beam", "Rock Slide"]},
+	},
+	"Scyther": {
+		"Swords Dance": {"level": 100, "moves": ["Twineedle", "Swords Dance", "Wing Attack", "Hyper Beam"]},
+	},
+	"Seadra": {
+		"Special Attacker": {"level": 100, "moves": ["Hydro Pump", "Blizzard", "Smokescreen", "Hyper Fang"]},
+	},
+	"Seaking": {
+		"Mixed Attacker": {"level": 100, "moves": ["Surf", "Blizzard", "Hyper Beam", "Double-Edge"]},
+	},
+	"Slowbro": {
+		"Tobybro": {"level": 100, "moves": ["Amnesia", "Surf", "Thunder Wave", "Rest"]},
+	},
+	"Snorlax": {
+		"OU Fishlax": {"level": 100, "moves": ["Body Slam", "Hyper Beam", "Earthquake", "Self-Destruct"]},
+		"OU Amnesialax": {"level": 100, "moves": ["Amnesia", "Blizzard", "Body Slam", "Rest"]},
+		"OU Reflectlax": {"level": 100, "moves": ["Body Slam", "Ice Beam", "Reflect", "Rest"]},
+	},
+	"Starmie": {
+		"OU Standard": {"level": 100, "moves": ["Blizzard", "Thunderbolt", "Recover", "Thunder Wave"]},
+	},
+	"Tangela": {
+		"Bind": {"level": 100, "moves": ["Bind", "Sleep Powder", "Stun Spore", "Mega Drain"]},
+	},
+	"Tauros": {
+		"OU Standard": {"level": 100, "moves": ["Body Slam", "Hyper Beam", "Earthquake", "Blizzard"]},
+	},
+	"Tentacruel": {
+		"Swords Dance + Wrap": {"level": 100, "moves": ["Swords Dance", "Wrap", "Poison Sting", "Surf"]},
+	},
+	"Vaporeon": {
+		"Growth": {"level": 100, "moves": ["Surf", "Acid Armor", "Rest", "Growth"]},
+	},
+	"Venomoth": {
+		"Status Spread": {"level": 100, "moves": ["Poison Sting", "Leech Life", "Sleep Powder", "Stun Spore"]},
+	},
+	"Venusaur": {
+		"Offensive Sleep Powder": {"level": 100, "moves": ["Razor Leaf", "Sleep Powder", "Swords Dance", "Poison Sting"]},
+	},
+	"Victreebel": {
+		"Offensive Sleep Powder": {"level": 100, "moves": ["Razor Leaf", "Sleep Powder", "Swords Dance", "Acid"]},
+	},
+	"Vileplume": {
+		"UU Swords Dance": {"level": 100, "moves": ["Sleep Powder", "Swords Dance", "Body Slam", "Mega Drain"]},
+	},
+	"Weezing": {
+		"Standard": {"level": 100, "moves": ["Sludge", "Explosion", "Thunderbolt", "Fire Blast"]},
+	},
+	"Wigglytuff": {
+		"Psychic Check": {"level": 100, "moves": ["Thunder Wave", "Body Slam", "Hyper Beam", "Psychic"]},
+		"Reflect": {"level": 100, "moves": ["Body Slam", "Psychic", "Reflect", "Rest"]},
+	},
+	"Zapdos": {
+		"Mixed Attacker": {"level": 100, "moves": ["Thunderbolt", "Whirlwind", "Thunder Wave", "Agility"]},
+	},
+};
