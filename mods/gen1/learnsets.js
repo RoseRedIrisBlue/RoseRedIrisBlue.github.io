39b579d39b3cc5c8a3046e5fde9ac51f12914297
KuraiTenshi26 <34247990+KuraiTenshi26@users.noreply.github.com>
2019-07-28 16:49:27 -0400

    hitmon buffs

--- mods/gen1/learnsets.js
+++ mods/gen1/learnsets.js
@@ -5093,6 +5093,7 @@ exports.BattleLearnsets = {
 		watergun: ["1M"],
 	}},
 	hitmonlee: {learnset: {
+		hyperbeam: ["1M"],
 		attract: ["2M"],
 		bide: ["1M"],
 		bodyslam: ["1M"],
@@ -5140,6 +5141,7 @@ exports.BattleLearnsets = {
 		toxic: ["1M", "2M"],
 	}},
 	hitmonchan: {learnset: {
+		hyperbeam: ["1M"],
 		agility: ["1L1", "2L7"],
 		attract: ["2M"],
 		bide: ["1M"],
